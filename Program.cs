fe8bdcf73806d800cd9c011b35e6c881fd89b34b
davidtav <david.mclaurel@outlook.com.br>
2026-05-10 23:42:59 -0300

    feat: adicionar integração do LiteDB para armazenamento de URLs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 using Carter;
+using LiteDB;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddCarter();
+builder.Services.AddSingleton<ILiteDatabase, LiteDatabase>(x=> new LiteDatabase("short.db"));
 var app = builder.Build();
 app.UseStaticFiles();
 app.MapCarter();
